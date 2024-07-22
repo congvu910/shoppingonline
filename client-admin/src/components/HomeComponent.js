b9d47154064d83f14260193a305b4e017675e04e
congvu910 <trancongvu2542003@gmail.com>
2024-07-23 00:02:47 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://elements-cover-images-0.imgix.net/c2eea1e1-4791-47be-8aae-0981def62c9e?auto=compress%2Cformat&w=1370&fit=max&s=ab47437acad304ea18e203b56b2db8d5" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
